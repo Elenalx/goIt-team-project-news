24fab1fde18bd68f94a8c385a3b905567f4c086e
Elenalx <107928893+Elenalx@users.noreply.github.com>
2023-02-23 17:44:52 +0000

    Deploying to gh-pages from @ Elenalx/goIt-team-project-news@fc4b4baccbb5c81ec644c4a934d1b2c8a90f44b2 🚀

--- favorite.211205b1.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector('[data-name="home"]'),a=document.querySelector('[data-name="favorite"]'),r=document.querySelector('[data-name="read"]');a.classList.add("navbar__link--current"),e.classList.remove("navbar__link--current"),r.classList.remove("navbar__link--current");
-//# sourceMappingURL=favorite.211205b1.js.map
