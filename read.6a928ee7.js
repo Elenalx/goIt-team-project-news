8f3b016b1ed259ebb82638164dd9e809a1dcaac8
Elenalx <107928893+Elenalx@users.noreply.github.com>
2023-02-23 22:02:19 +0000

    Deploying to gh-pages from @ Elenalx/goIt-team-project-news@55bb3b574c74bf0f55084f09cc9e9432d1fa61fd 🚀

--- read.6a928ee7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector('[data-name="home"]'),a=document.querySelector('[data-name="favorite"]');document.querySelector('[data-name="read"]').classList.add("navbar__link--current"),a.classList.remove("navbar__link--current"),e.classList.remove("navbar__link--current")}();
-//# sourceMappingURL=read.6a928ee7.js.map
--- read.d18fa217.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector('[data-name="home"]'),a=document.querySelector('[data-name="favorite"]');document.querySelector('[data-name="read"]').classList.add("navbar__link--current"),a.classList.remove("navbar__link--current"),e.classList.remove("navbar__link--current");
-//# sourceMappingURL=read.d18fa217.js.map
