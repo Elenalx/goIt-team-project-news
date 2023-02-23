f1715660b8ce0e913eca26215a4c03932ef93353
NikolaiOD <110899590+NikolaiOD@users.noreply.github.com>
2023-02-23 23:52:47 +0200

    update

--- src/js/read-page.js
+++ src/js/read-page.js
@@ -2,6 +2,16 @@ const homeLinkEl = document.querySelector('[data-name="home"]');
 const favoriteLinkEl = document.querySelector('[data-name="favorite"]');
 const readLinkEl = document.querySelector('[data-name="read"]');
 
-readLinkEl.classList.add('navbar__link--current');
-favoriteLinkEl.classList.remove('navbar__link--current');
-homeLinkEl.classList.remove('navbar__link--current');
+const homeMobileLinkEl = document.querySelector('[data-name="home-mobile"]');
+const favoriteMobileLinkEl = document.querySelector(
+  '[data-name="favorite-mobile"]'
+);
+const readMobileLinkEl = document.querySelector('[data-name="read-mobile"]');
+
+readLinkEl.classList.add('navbarlink--current');
+favoriteLinkEl.classList.remove('navbarlink--current');
+homeLinkEl.classList.remove('navbarlink--current');
+
+readMobileLinkEl.classList.add('navbar-mobilelink--current');
+homeMobileLinkEl.classList.remove('navbar-mobilelink--current');
+favoriteMobileLinkEl.classList.remove('navbar-mobilelink--current');
